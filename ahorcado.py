8950b6243afe756efae0921c6a3a2d9c79b4f3ce
Paula Daniela <paula.velosa2704@gmail.com>
2021-04-20 15:09:11 -0500

    ahora se escribe las letras 2 veces por palabra

--- ahorcado.py
+++ ahorcado.py
@@ -31,33 +31,49 @@ def get_word():
             words.append(word) 
         position = random.randint(0, (len(words)-1))
         return words[position]
-def print_word(word, dict_word, dict_new_word, position):
-    printing = ["_ " for i in range (len(word)-1)]
-    print(printing)
-    letter_guessed = dict_word.get(position)
-    print(letter_guessed)
+def letters_choice(word, letters_chosen = []):
+    new_word = word
+    while len(new_word) >= 0:
+        print(word)
+        letter_chosen = input("ingresa la letra = ")
+        letters_chosen.append(letter_chosen)
+        print(f'letters= {letters_chosen}')
+        letter_match = list(map(lambda letter : letter == letter_chosen, word))
+        new_word = list((filter(lambda letter : letter == letter_chosen, word)))
+        verify_word(new_word, letter_match, letters_chosen)
+        
+           
 
-def verify_word(word, dict_word, dict_new_word = {}):
-    print_word(word, dict_word, dict_new_word, position = None)
-    letter_chosen = input("ingresa la letra = ")
-    if map(lambda letter : letter != letter_chosen , word):
-        new_word = list(filter(lambda letter : letter != letter_chosen , word))
-        positions = list(map(lambda letter : letter != letter_chosen , word))
-        for position in positions:
-            if position == False :
-                print_word(word, dict_word, dict_new_word,position)
-        print(positions)
-    print("prueba otra vez")
-    return[new_word]    
+
+
+
+def verify_word(word, letter_match, letters_chosen):
+    word_dict = dict(filter(lambda letter : letter, enumerate(word)))
+    positions=[]
+    for letter_chosen in letters_chosen:
+        for keys, values in word_dict.items():
+            if letter_chosen == values:
+                positions.append(keys) 
+        printing = [] 
+        
+    for letter in letter_match:
+        if letter:
+            for position in positions:
+                printing.append(word_dict.get(position))
+                positions.remove(position)
+        printing.append("_")
+        
+    print(" ".join(printing))
     
-
+    
+    
     
 def screen():
     print('bienvenido a ahorcado!')
     word = get_word()
-    word_dict ={letter for letter in sorted(enumerate(word))}
-    print(word_dict)
-    verify_word(word, word_dict)
+    #word_dict ={letter for letter in sorted(enumerate(word))}
+    #print(word_dict)
+    letters_choice(word)
     print('adivina la letra')
     #draw()
 
